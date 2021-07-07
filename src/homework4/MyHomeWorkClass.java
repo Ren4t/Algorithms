379417ad81fbf984de66fd6ca96dc9971b52b840
Ren4t <ren4tab@yandex.ru>
2021-07-07 19:09:23 +0300

    homework4 create class MyHomeWorkClass and Stack and iter #2

---
+++ src/homework4/MyHomeWorkClass.java
@@ -0,0 +1,18 @@
+package homework4;
+
+public class MyHomeWorkClass {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.put(1);
+        stack.put(2);
+        stack.put(3);
+        stack.put(4);
+        System.out.println(stack);
+
+        System.out.println(stack.pick());
+        System.out.println(stack.pick());
+
+        System.out.println(stack);
+
+    }
+}
